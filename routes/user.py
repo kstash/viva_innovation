c2ab2f824a4d87cefbc20a47e752f695f2cfab5a
Seungtae Kim <25264932+kstash@users.noreply.github.com>
2024-04-25 23:54:24 +0900

    [fix] updating user info can only get called by it's owner(jwt check)

--- routes/user.py
+++ routes/user.py
@@ -1,10 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Body, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException
 from pydantic import EmailStr
 from rest_framework import status
 from passlib.context import CryptContext
 
-from auth.jwt_handler import sign_jwt
+from auth.jwt_handler import get_current_user, sign_jwt
 from models.user import User
 from schemas.user import UserLogin, UserOut, UserUpdate
 
@@ -13,13 +13,13 @@ user = APIRouter(prefix='/user')
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 
-@user.get('/', response_description='Get all users', response_model=List[UserOut])
+@user.get('/', response_description='Get all users', response_model=List[UserOut], tags=["user"])
 async def find_all_users() -> list:
     users = await User.all().to_list()
     return users
 
 
-@user.post('/', response_description='Created user', response_model=UserOut)
+@user.post('/', response_description='Created user', response_model=UserOut, tags=["user"])
 async def create_user(user: User = Body(...)):
     user_exists = await User.find_one(User.email == user.email)
     if user_exists:
@@ -34,7 +34,7 @@ async def create_user(user: User = Body(...)):
     return new_user
 
 
-@user.post('/login', response_description='Login user')
+@user.post('/login', response_description='Login user', tags=["user"])
 async def create_user(user: UserLogin = Body(...)):
     user_exists = await User.find_one(User.email == user.email)
     if user_exists:
@@ -48,11 +48,14 @@ async def create_user(user: UserLogin = Body(...)):
     )
 
 
-# TODO: 사용자 본인만 수정 가능하도록 추가
-@user.put('/{user_email}', response_description='Updated user', response_model=UserOut)
-async def update_user(user_email: EmailStr, user: UserUpdate):
+@user.put('/{user_email}', response_description='Updated user', response_model=UserOut, tags=["user"])
+async def update_user(user_email: EmailStr, user: UserUpdate, current_user: User = Depends(get_current_user)):
     user_exists = await User.find_one(User.email == user_email)
     if user_exists:
+        # 사용자 본인만 사용자 정보 수정 가능
+        if user_exists.email != current_user.email:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authorized")
+
         user_dict = user.model_dump()
         for field, value in user_dict.items():
             # 비밀번호는 해싱해서 저장
